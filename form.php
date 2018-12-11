67c61e1d396f34bc67226385f1d82a2a86a44b32
Stabmirz <amirza@techlaunch.io>
2018-12-10 21:49:08 -0500

    first commit

---
+++ form.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+<form action="registration.php" method="post">
+Username: <input type="text" name="name">
+Password: <input type="password" name="password">
+<input type="submit">
+</form>
+
+</body>
+</html>
