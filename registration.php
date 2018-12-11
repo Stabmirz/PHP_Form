135edb36bb3a4b0c2b2961742262fd628bc20f7d
Stabmirz <amirza@techlaunch.io>
2018-12-10 21:56:54 -0500

    first commit

--- registration.php
+++ registration.php
@@ -1,7 +1,5 @@
-<html>
-<body>
-
-Welcome <?php echo $_POST["name"]; ?>!
-
-</body>
-</html>
+<?php
+$str = "<h1>name</h1>";
+$newstr = filter_var($str, FILTER_SANITIZE_STRING);
+echo "<h3>Welcome $_POST[$newstr]!</h3>";
+?>
